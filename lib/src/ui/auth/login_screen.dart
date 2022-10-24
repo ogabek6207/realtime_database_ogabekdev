30aae0d3832fad50a4010ce0bbf0441834e5513c
ogabe <ogabekabdijabborov@gmail.com>
2022-10-24 20:18:46 +0500

    utils

--- lib/src/ui/auth/login_screen.dart
+++ lib/src/ui/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:realtime_database_ogabekdev/src/color/app_color.dart';
+import 'package:realtime_database_ogabekdev/src/utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,14 +13,17 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    double h = Utils.getHeight(context);
+    double w = Utils.getWidth(context);
     return Scaffold(
       body: ListView(
+        padding: EdgeInsets.only(left: 25*w, right: 25*w),
         children: [
-          SizedBox(
-            height: 68,
+           SizedBox(
+            height: 68*h,
           ),
           SvgPicture.asset('assets/icons/zetgramm.svg'),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Row(
@@ -28,10 +32,13 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: 233,
                 child: Row(
                   children: [
-                    Text(
+                     Text(
                       'Log In',
                       style: TextStyle(
                         fontFamily: AppColor.fontFamilyNunitoSans,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                        color: Color(0xFF383838)
                       ),
                     )
                   ],
